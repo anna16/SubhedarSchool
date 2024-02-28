741554f8ebbe3ad3d9b21a68084a9a3964bdf35d
Anagha <anagha.renghe@noveline.co.in>
2024-02-28 17:59:29 +0530

    Hasrouter changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import ReactDOM from "react-dom/client";
-import { HashRouter,BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 // import Blogs from "./pages/Blogs";
 // import Contact from "./pages/Contact";
- import AboutFounder from "./pages/AboutFounderPage";
- import AboutGEIDadar from "./pages/AboutGEIDadarPage";
- import AboutSWSchool from "./pages/AboutSWSchoolPage";
- import CommitteeMembers from "./pages/CommitteeMembersPage";
- import Acadmic from "./pages/AcadmicPage";
- import Sports from "./pages/SportsPage";
+import AboutFounder from "./pages/AboutFounderPage";
+import AboutGEIDadar from "./pages/AboutGEIDadarPage";
+import AboutSWSchool from "./pages/AboutSWSchoolPage";
+import CommitteeMembers from "./pages/CommitteeMembersPage";
+import Acadmic from "./pages/AcadmicPage";
+import Sports from "./pages/SportsPage";
 import EnquiryForm from "./pages/EnquiryFormPage"
 import Teacher from "./pages/TeacherPage"
 import PhotoGallery from "./pages/PhotoGalleryPages";
@@ -52,20 +52,20 @@ function App() {
     loadScript('https://anna16.github.io/SubhedarSchool/js/main.js')
   }, [])
   return (
-    <BrowserRouter basename="/SubhedarSchool">
+    <BrowserRouter basename="https://anna16.github.io/SubhedarSchool">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route  path="about" element={<AboutFounder />} />
-        <Route  path="aboutGEIDadar" element={<AboutGEIDadar />} />
-        <Route  path="aboutSWSchool" element={<AboutSWSchool />} />
-        <Route  path="committeeMembers" element={<CommitteeMembers />} />
-        <Route  path="acadmic" element={<Acadmic />} />
-        <Route  path="sports" element={<Sports />} />
-        <Route  path="enquiryForm" element={<EnquiryForm />} />
-        <Route  path="teacher" element={<Teacher />} />
-        <Route  path="photoGallery" element={<PhotoGallery />} />
-        <Route  path="schoolTour" element={<SchoolTour />} />
-        <Route  path="contact" element={<Contact />} />
+        <Route path="about" element={<AboutFounder />} />
+        <Route path="aboutGEIDadar" element={<AboutGEIDadar />} />
+        <Route path="aboutSWSchool" element={<AboutSWSchool />} />
+        <Route path="committeeMembers" element={<CommitteeMembers />} />
+        <Route path="acadmic" element={<Acadmic />} />
+        <Route path="sports" element={<Sports />} />
+        <Route path="enquiryForm" element={<EnquiryForm />} />
+        <Route path="teacher" element={<Teacher />} />
+        <Route path="photoGallery" element={<PhotoGallery />} />
+        <Route path="schoolTour" element={<SchoolTour />} />
+        <Route path="contact" element={<Contact />} />
         {/*
         
         <Route path="donate" element={<Donate />} /> */}
