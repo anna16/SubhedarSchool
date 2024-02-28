817e5a6ba241f1ecc4fc63fd9688ca1a17368226
Anagha <anagha.renghe@noveline.co.in>
2024-02-28 17:35:20 +0530

    Hasrouter changes

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ function App() {
     loadScript('https://anna16.github.io/SubhedarSchool/js/main.js')
   }, [])
   return (
-    <HashRouter>
+    <HashRouter basename="/SubhedarSchool">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<AboutFounder />} />
